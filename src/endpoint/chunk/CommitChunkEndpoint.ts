899c294c8a281f7e7577735c03e3e30178ac93ab
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-04 22:57:18 +0100

    Common: Improved chunk stuff

--- src/endpoint/chunk/CommitChunkEndpoint.ts
+++ src/endpoint/chunk/CommitChunkEndpoint.ts
@@ -1,6 +1,6 @@
-import {IChunkCommit, IChunkService, IEndpoint, IFile} from "@leight-core/api";
+import {IChunkCommit, IChunkCommitEvent, IChunkService, IEndpoint, IFile} from "@leight-core/api";
 import {IChunkEndpointQuery} from "@leight-core/server";
 
-export const CommitChunkEndpoint: (chunkService: IChunkService) => IEndpoint<"Commit", IChunkCommit, IFile, IChunkEndpointQuery> = chunkService => async ({req: {body}, query: {chunkId}}) => {
-	return chunkService.commit(chunkId, body);
+export const CommitChunkEndpoint: (chunkService: IChunkService, chunkCommitEvent: IChunkCommitEvent) => IEndpoint<"Commit", IChunkCommit, IFile, IChunkEndpointQuery> = (chunkService, chunkCommitEvent) => async params => {
+	return chunkCommitEvent(chunkService.commit(params.query.chunkId, params.req.body), params);
 }
