bb1faf8c9dc3c2d9966e7ccfe960556246a92099
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-04 05:44:39 +0100

    Common: Version bump

--- src/generators/generateMutationEndpoint.ts
+++ src/generators/generateMutationEndpoint.ts
@@ -7,7 +7,7 @@ export function generateMutationEndpoint(sdk: ISdk): string {
 	// language=text
 	return cleanup(`
 import {FC} from "react";
-import {useLinkContext, Form, IFormProps, useLinkContext, createMutationHook, createPromiseHook} from "@leight-core/client";	
+import {Form, IFormProps, useLinkContext, createMutationHook, createPromiseHook} from "@leight-core/client";	
 import {AxiosRequestConfig} from "axios";
 ${sdk.imports.map(_import => `import {${_import.imports.join(", ")}} from ${_import.from};`).join("\n")}
 
