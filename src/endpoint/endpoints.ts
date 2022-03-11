298dee33773bc21e8168a5cc04dd4df61d54d2f1
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-11 15:55:27 +0100

    Common: Updated query

--- src/endpoint/endpoints.ts
+++ src/endpoint/endpoints.ts
@@ -1,7 +1,7 @@
-import {ICreateEndpoint, IDeleteEndpoint, IEndpoint, IEndpointCallback, IFetchEndpoint, IListEndpoint, IMutationEndpoint, IPatchEndpoint, IQuery, IQueryEndpoint, IQueryParams, IQueryResult} from "@leight-core/api";
+import {ICreateEndpoint, IDeleteEndpoint, IEndpoint, IEndpointCallback, IFetchEndpoint, IFilter, IListEndpoint, IMutationEndpoint, IOrderBy, IPatchEndpoint, IQuery, IQueryEndpoint, IQueryParams, IQueryResult} from "@leight-core/api";
 import getRawBody from "raw-body";
 
-export const Endpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
+export const Endpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams | void = void>(handler: IEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
 	return async (req, res) => {
 		try {
 			const response = await handler({
@@ -18,30 +18,30 @@ export const Endpoint = <TName extends string, TRequest, TResponse, TQuery exten
 	};
 }
 
-export const FetchEndpoint = <TName extends string, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IFetchEndpoint<TName, TResponse, TQuery>): IEndpointCallback<TName, void, TResponse, TQuery> => {
+export const FetchEndpoint = <TName extends string, TResponse, TQuery extends IQueryParams | void = void>(handler: IFetchEndpoint<TName, TResponse, TQuery>): IEndpointCallback<TName, void, TResponse, TQuery> => {
 	return Endpoint<TName, void, TResponse, TQuery>(handler);
 }
 
-export const ListEndpoint = <TName extends string, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IListEndpoint<TName, TResponse, TQuery>): IEndpointCallback<TName, void, TResponse, TQuery> => {
+export const ListEndpoint = <TName extends string, TResponse, TQuery extends IQueryParams | void = void>(handler: IListEndpoint<TName, TResponse, TQuery>): IEndpointCallback<TName, void, TResponse, TQuery> => {
 	return Endpoint<TName, void, TResponse, TQuery>(handler);
 }
 
-export const MutationEndpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IMutationEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
+export const MutationEndpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams | void = void>(handler: IMutationEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
 	return Endpoint<TName, TRequest, TResponse, TQuery>(handler);
 }
 
-export const CreateEndpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: ICreateEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
+export const CreateEndpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams | void = void>(handler: ICreateEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
 	return Endpoint<TName, TRequest, TResponse, TQuery>(handler);
 }
 
-export const PatchEndpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IPatchEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
+export const PatchEndpoint = <TName extends string, TRequest, TResponse, TQuery extends IQueryParams | void = void>(handler: IPatchEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, TResponse, TQuery> => {
 	return Endpoint<TName, TRequest, TResponse, TQuery>(handler);
 }
 
-export const QueryEndpoint = <TName extends string, TRequest extends IQuery, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IQueryEndpoint<TName, TRequest, TResponse, TQuery>): IEndpointCallback<TName, TRequest, IQueryResult<TResponse>, TQuery> => {
+export const QueryEndpoint = <TName extends string, TRequest extends IQuery<TFilter, TOrderBy> | void, TResponse, TFilter extends IFilter | void = void, TOrderBy extends IOrderBy | void = void, TQuery extends IQueryParams | void = void>(handler: IQueryEndpoint<TName, TRequest, TResponse, TFilter, TOrderBy, TQuery>): IEndpointCallback<TName, TRequest, IQueryResult<TResponse>, TQuery> => {
 	return Endpoint<TName, TRequest, IQueryResult<TResponse>, TQuery>(handler);
 }
 
-export const DeleteEndpoint = <TName extends string, TResponse, TQuery extends IQueryParams = IQueryParams>(handler: IDeleteEndpoint<TName, TResponse, TQuery>): IEndpointCallback<TName, void, TResponse, TQuery> => {
+export const DeleteEndpoint = <TName extends string, TResponse, TQuery extends IQueryParams | void = void>(handler: IDeleteEndpoint<TName, TResponse, TQuery>): IEndpointCallback<TName, void, TResponse, TQuery> => {
 	return Endpoint<TName, void, TResponse, TQuery>(handler);
 }
