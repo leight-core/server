0ca9522b9f28d780a430f650e27a3be0533b7d08
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-18 17:06:51 +0100

    Common: Fix

--- src/generators/generateFetchEndpoint.ts
+++ src/generators/generateFetchEndpoint.ts
@@ -22,6 +22,7 @@ export function generateFetchEndpoint(sdk: ISdk): string {
 				'EntityProvider',
 				'IQueryProps',
 				'Query',
+				'useQueryClient',
 			],
 			from: '"@leight-core/client"'
 		},
