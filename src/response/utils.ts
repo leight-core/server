0e3e12a4e120273a66ef027e50856311bd011092
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-02 00:08:15 +0200

    Common: Version bump

--- src/response/utils.ts
+++ src/response/utils.ts
@@ -1,3 +1,5 @@
 import {NextApiResponse} from "next";
 
-export const withConflict = <T>(response: NextApiResponse, cb?: T) => response.status(409).end(cb);
+export const withConflict = async <T>(response: NextApiResponse, cb?: T) => {
+	response.status(409).end(cb);
+};
