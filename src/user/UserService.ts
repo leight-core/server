ba4e16dee9379b18e82bf2f271cf4b6e1b69153d
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-10 10:56:31 +0200

    Common: Version bump

--- src/user/UserService.ts
+++ src/user/UserService.ts
@@ -1,11 +1,11 @@
 import {IUserService} from "@leight-core/api";
 
-export const UserService = (userId?: string): IUserService => ({
+export const UserService = (userId?: string | null): IUserService => ({
 	getUserId: () => {
 		if (!userId) {
 			throw new Error("User not available");
 		}
 		return userId;
 	},
-	getOptionalUserId: () => userId,
+	getOptionalUserId: () => userId || undefined,
 });
