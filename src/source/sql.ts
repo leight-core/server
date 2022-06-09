2b62678bd8ac351977616e5e9af76253fd138a47
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-09 13:51:49 +0200

    Common: Version bump

--- src/source/sql.ts
+++ src/source/sql.ts
@@ -2,7 +2,10 @@ import {Sql} from "@leight-core/utils";
 import {PrismaClient} from "@prisma/client";
 
 export const runSql = (source: string, prisma: PrismaClient, timeout: number = 1000 * 60) => {
-	return prisma.$transaction(async prisma => Sql.execute(source, prisma.$executeRawUnsafe), {
+	return prisma.$transaction(async prisma => Sql.execute(source, sql => {
+		console.log(`Executing: ${sql}`);
+		return prisma.$executeRawUnsafe(sql);
+	}), {
 		timeout,
 	});
 };
