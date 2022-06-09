4d1e9e0334a47f6417543f4999ace289f5a1257b
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-09 14:26:53 +0200

    Common: Version bump

--- src/source/sql.ts
+++ src/source/sql.ts
@@ -1,5 +1,27 @@
-import {sqlExecute} from "@leight-core/utils";
 import {PrismaClient} from "@prisma/client";
+import fs from "fs";
+
+export const sqlParse = (source: string): string[] => {
+	return source
+		.replace(/(--)(.*)/g, "")
+		.replace(/\r?\n|\r/g, " ")
+		.replace(/\/\*.*\*\//g, " ")
+		.replace(/\s\s+/g, " ")
+		.split(";")
+		.map(query => query.trim())
+		.filter(query => query.length);
+};
+
+export const sqlRead = (file: string): string[] => sqlParse(fs.readFileSync(file).toString());
+
+export const sqlExecute = async <T>(file: string, executor: (query: string) => Promise<T>): Promise<T[]> => {
+	const queries = sqlRead(file);
+	const results: T[] = [];
+	for (const query of queries) {
+		results.push(await executor(query));
+	}
+	return results;
+};
 
 export const runSql = (source: string, prisma: PrismaClient, timeout: number = 1000 * 60) => {
 	return prisma.$transaction(async prisma => sqlExecute(source, sql => {
