0d9d595e239b0360f370980e915754b50d1f48c7
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-14 12:03:36 +0200

    Common: Version bump

--- src/source/Source.ts
+++ src/source/Source.ts
@@ -3,10 +3,8 @@ import {
 	IImportHandlers,
 	IPrismaTransaction,
 	IPromiseMapper,
-	IQuery,
 	IQueryFilter,
 	ISource,
-	ISourceAcl,
 	ISourceCreate,
 	ISourceEntity,
 	ISourceFetch,
@@ -22,7 +20,6 @@ import {PromiseMapper} from "@leight-core/utils";
 import LRUCache from "lru-cache";
 import {GetServerSideProps} from "next";
 import crypto from "node:crypto";
-import {ParsedUrlQuery} from "querystring";
 
 export abstract class AbstractSource<TSource extends ISource<any, any, any>> implements ISource<ISourceCreate<TSource>, ISourceEntity<TSource>, ISourceItem<TSource>, ISourceQuery<TSource>, ISourceFetch<TSource>, ISourceFetchParams<TSource>> {
 	readonly mapper: IPromiseMapper<ISourceEntity<TSource>, ISourceItem<TSource>>;
@@ -68,6 +65,22 @@ export abstract class AbstractSource<TSource extends ISource<any, any, any>> imp
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	async $patch(patch: UndefinableOptional<ISourceCreate<TSource>> & IWithIdentity): Promise<ISourceEntity<TSource>> {
 		throw new Error(`Source [${this.name}] does not support item patching.`);
+	}
+
+	import(create: ISourceCreate<TSource>): Promise<ISourceEntity<TSource>> {
+		try {
+			return this.$create(create);
+		} catch (e) {
+			return onUnique(e, () => this.$patch({
+				id: this.createToId(create),
+				...create,
+			}));
+		}
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	createToId(create: ISourceCreate<TSource>): string | undefined {
+		throw new Error(`Source [${this.name}] does not support mapping Create object to an ID.`);
 	}
 
 	async remove(ids: string[]): Promise<ISourceEntity<TSource>[]> {
@@ -142,7 +155,7 @@ export abstract class AbstractSource<TSource extends ISource<any, any, any>> imp
 		return {
 			[this.name]: () => ({
 				withUser: this.withUser,
-				handler: this.create,
+				handler: this.import,
 			}),
 		};
 	}
@@ -194,206 +207,3 @@ export abstract class AbstractSource<TSource extends ISource<any, any, any>> imp
 
 	abstract map(source: ISourceEntity<TSource>): Promise<ISourceItem<TSource>>;
 }
-
-export interface ISourceRequest<TCreate, TEntity, TItem, TQuery extends IQuery = IQuery, TFetch extends Record<string, any> = any, TFetchParams extends ParsedUrlQuery = any> {
-	name: string;
-	prisma: IPrismaTransaction;
-	source?: Omit<Partial<ISource<TCreate, TEntity, TItem, TQuery, TFetch, TFetchParams>>, "name" | "prisma">;
-	cache?: {
-		count?: LRUCache<string, number>;
-		query?: LRUCache<string, TEntity[]>;
-	};
-	acl?: ISourceAcl;
-
-	map(source: TEntity): Promise<TItem>;
-}
-
-export const Source = <T extends ISource<any, any, any>>(
-	{
-		name,
-		prisma,
-		source: {
-			create: $create = async () => {
-				throw new Error(`Source [${name}] does not support item creation.`);
-			},
-			patch: $patch = async () => {
-				throw new Error(`Source [${name}] does not support item patching.`);
-			},
-			remove: $delete = async () => {
-				throw new Error(`Source [${name}] does not support item deletion.`);
-			},
-			get: $get = async () => {
-				throw new Error(`Source [${name}] does not support getting an item by an id.`);
-			},
-			find: $find = async () => {
-				throw new Error(`Source [${name}] does not support finding an item by a query.`);
-			},
-			query: $query = async () => {
-				throw new Error(`Source [${name}] does not support querying items.`);
-			},
-			count: $count = async () => {
-				throw new Error(`Source [${name}] does not support counting items by a query.`);
-			},
-			withFilter: $withFilter = ({filter}: ISourceQuery<T>) => filter,
-			clearCache: $clearCache,
-			...source
-		} = {},
-		acl,
-		map,
-		cache,
-		...request
-	}: ISourceRequest<ISourceCreate<T>, ISourceEntity<T>, ISourceItem<T>, ISourceQuery<T>, ISourceFetch<T>, ISourceFetchParams<T>> & Omit<T, keyof ISource<ISourceCreate<T>, ISourceEntity<T>, ISourceItem<T>, ISourceQuery<T>, ISourceFetch<T>, ISourceFetchParams<T>>>): T => {
-	const $mapper = PromiseMapper(map);
-	let $prisma = prisma;
-	let $user = User();
-
-	if (acl?.lock) {
-		(acl.default = (acl.default || [])).push(
-			`*`,
-		);
-		(acl.create = (acl.create || [])).push(
-			`${name}.create`,
-			`${name}.write`,
-		);
-		(acl.mapper = (acl.mapper || [])).push(
-			`${name}.mapper`,
-			`${name}.read`,
-		);
-		(acl.patch = (acl.patch || [])).push(
-			`${name}.patch`,
-			`${name}.write`,
-		);
-		(acl.remove = (acl.remove || [])).push(
-			`${name}.remove`,
-		);
-		(acl.get = (acl.get || [])).push(
-			`${name}.get`,
-			`${name}.read`,
-		);
-		(acl.find = (acl.find || [])).push(
-			`${name}.find`,
-			`${name}.read`,
-		);
-		(acl.query = (acl.query || [])).push(
-			`${name}.query`,
-			`${name}.read`,
-		);
-		(acl.count = (acl.count || [])).push(
-			`${name}.count`,
-			`${name}.read`,
-		);
-		(acl.fetch = (acl.fetch || [])).push(
-			`${name}.fetch`,
-			`${name}.read`,
-		);
-	}
-
-	const $source: ISource<ISourceCreate<T>, ISourceEntity<T>, any, ISourceQuery<T>, ISourceFetch<T>, ISourceFetchParams<T>> = {
-		name,
-		get prisma() {
-			return $prisma;
-		},
-		get mapper() {
-			$source.user.checkAny((acl?.default || []).concat(acl?.mapper || []));
-			return $mapper;
-		},
-		get user() {
-			return $user;
-		},
-		create: async create => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.create || []));
-			try {
-				const result = await $create(create);
-				await $source.clearCache();
-				return result;
-			} catch (e) {
-				return onUnique(e, async () => {
-					throw new ClientError(`Unique error on [${name}].`, 409);
-				});
-			}
-		},
-		patch: async patch => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.patch || []));
-			const result = await $patch(patch);
-			await $source.clearCache();
-			return result;
-		},
-		remove: async ids => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.remove || []));
-			const result = await $delete(ids);
-			await $source.clearCache();
-			return result;
-		},
-		get: async id => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.get || []));
-			return $get(id);
-		},
-		find: async query => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.find || []));
-			return $find(query);
-		},
-		query: async query => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.query || []));
-			const hash = $source.hashOf(query, "query");
-			if (!cache?.query?.has(hash)) {
-				cache?.query?.set(hash, await $query(query));
-			}
-			return cache?.query?.get(hash) || $query(query);
-		},
-		count: async query => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.count || []));
-			const hash = $source.hashOf(query, "count");
-			if (!cache?.count?.has(hash)) {
-				cache?.count?.set(hash, await $count(query));
-			}
-			return cache?.count?.get(hash) || $count(query);
-		},
-		withFilter: $withFilter,
-		fetch: async query => {
-			$source.user.checkAny((acl?.default || []).concat(acl?.fetch || []));
-			try {
-				return await $source.find(query);
-			} catch (e) {
-				console.warn(e);
-				return null;
-			}
-		},
-		importers: () => ({
-			[name]: () => ({
-				withUser: $source.withUser,
-				handler: $source.create,
-			}),
-		}),
-		withUser: user => {
-			$user = user;
-			return $source;
-		},
-		withPrisma: prisma => {
-			$prisma = prisma;
-			return $source;
-		},
-		ofSource: source => {
-			$source.withPrisma(source.prisma);
-			$source.withUser(source.user);
-			return $source;
-		},
-		withFetch: (key, query) => withFetch<ISourceFetch<T>, ISourceFetchParams<T>, ISource<ISourceCreate<T>, ISourceEntity<T>, any, ISourceQuery<T>, ISourceFetch<T>, ISourceFetchParams<T>>>($source)(key, query),
-		map,
-		mapNull: async source => source ? map(source) : undefined,
-		list: $mapper.list,
-		hashOf: (query, type) => crypto.createHash("sha256").update(JSON.stringify({
-			query,
-			type,
-			userId: $source.user.optional(),
-		})).digest("hex"),
-		clearCache: async () => {
-			cache?.query?.clear();
-			cache?.count?.clear();
-			return $clearCache?.();
-		},
-		...request,
-		...source,
-	};
-
-	return $source as ISource<ISourceCreate<T>, ISourceEntity<T>, ISourceItem<T>, ISourceQuery<T>, ISourceFetch<T>, ISourceFetchParams<T>> & T;
-};
