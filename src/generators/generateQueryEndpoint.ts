e865755e111765c98a48e4af0cf59cc928e1f2a8
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-19 13:18:08 +0200

    Common: Version bump

--- src/generators/generateQueryEndpoint.ts
+++ src/generators/generateQueryEndpoint.ts
@@ -236,82 +236,64 @@ export interface I${name}SourceSelectProps extends IQuerySourceSelectProps<${res
 
 export const ${name}SourceSelect: FC<I${name}SourceSelectProps> = ({providerProps, selectionList, selectionProps, selectionProvider, selectionDrawer, ...props}) => {
 	const formItem = useOptionalFormItemContext();
-	const selection = useRef<Record<string, ${response}>>({});
-	return selectionList ? <Input.Group>
-		<Row>
-			<Col flex={"auto"}> 
-				<${name}Provider {...providerProps}>
-					<QuerySourceSelect<${response}>
-						onSelect={({entity}) => {
-							selection.current[entity.id] = entity;
-						}}
-						onDeselect={({entity}) => {
-							delete selection.current[entity.id];
-						}}
-						onClear={() => {
-							selection.current = {};
-						}}
-						{...props}
-					/>
-				</${name}Provider>
-			</Col>
-			<Col push={0}>
-				<DrawerButton
-					icon={<SelectOutlined/>}
-					title={"common.selection.${name}.title"}
-					size={props.size}
-					tooltip={"common.selection.${name}.title.tooltip"}
-					width={800}
-					type={"text"}
-					{...selectionDrawer}
-				>
-					<DrawerContext.Consumer>
-						{drawerContext => <${name}ProviderControl
-							defaultSize={10}
-							{...selectionProvider}
-						>
-							<SelectionProvider<${response}>
-								type={"single"}
-								applySelection={selection.current}
-								onSelection={({selected, items}) => {
-									formItem?.setValue(selected);
-									formItem?.setErrors([]);
-									selection.current = items;
-									drawerContext.close();
-								}}
-								{...selectionProps}
+		return selectionList ? <SelectionProvider<${response}>
+		type={"single"}
+		onSelection={({selected}) => {
+			formItem?.setValue(selected);
+			formItem?.setErrors([]);
+		}}
+		{...selectionProps}
+	>
+		<SelectionContext.Consumer>
+			{selectionContext => <>
+				<Input.Group>
+					<Row>
+						<Col flex={"auto"}>
+							<${name}Provider {...providerProps}>
+								<QuerySourceSelect<${response}>
+									onSelect={({entity}) => selectionContext.item(entity)}
+									onDeselect={({entity}) => selectionContext.deItem(entity)}
+									onClear={() => selectionContext.clear()}
+									{...props}
+								/>
+							</${name}Provider>
+						</Col>
+						<Col push={0}>
+							<DrawerButton
+								icon={<SelectOutlined/>}
+								title={"common.selection.${name}.title"}
+								size={props.size}
+								tooltip={"common.selection.${name}.title.tooltip"}
+								width={800}
+								type={"text"}
+								{...selectionDrawer}
 							>
-								<SelectionContext.Consumer>
-									{selectionContext => <>
+								<DrawerContext.Consumer>
+									{drawerContext => <${name}ProviderControl
+										defaultSize={10}
+										{...selectionProvider}
+									>
 										<BubbleButton
 											icon={<CheckOutline fontSize={32}/>}
-											onClick={() => selectionContext.handleSelection()}
+											onClick={() => {
+												selectionContext.handleSelection();
+												drawerContext.close();
+											}}
 										/>
 										{selectionList({
 											selectionContext,
 											drawerContext,
 										})}
-									</>}
-								</SelectionContext.Consumer>
-							</SelectionProvider>
-						</${name}ProviderControl>}
-					</DrawerContext.Consumer>
-				</DrawerButton>
-			</Col>
-		</Row>
-	</Input.Group> : <${name}Provider {...providerProps}>
-		<QuerySourceSelect<${response}>
-			onSelect={({entity}) => {
-				selection.current[entity.id] = entity;
-			}}
-			onDeselect={({entity}) => {
-				delete selection.current[entity.id];
-			}}
-			onClear={() => {
-				selection.current = {};
-			}}
-			{...props}
-		/>
+									</${name}ProviderControl>}
+								</DrawerContext.Consumer>
+							</DrawerButton>
+						</Col>
+					</Row>
+				</Input.Group>
+			</>}
+		</SelectionContext.Consumer>
+	</SelectionProvider> : <${name}Provider {...providerProps}>
+		<QuerySourceSelect<${response}> {...props}/>
 	</${name}Provider>;
 };
 
