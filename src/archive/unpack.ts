b96dd8082f4ad89e0f1a75e0ca4eeee6d0502213
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-11 19:52:23 +0200

    Common: Version bump

--- src/archive/unpack.ts
+++ src/archive/unpack.ts
@@ -4,8 +4,7 @@ import tar from "tar";
 export const unpack = (archive: string, target: string) => {
 	fs.mkdirSync(target, {recursive: true});
 	return tar.x({
-		strip: 1,
-		file:  archive,
-		cwd:   target,
+		file: archive,
+		cwd:  target,
 	});
 };
