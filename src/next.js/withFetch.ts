08c35b30331d0ff6ed68625836c472c9de8ae687
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-12 14:34:09 +0200

    Common: Version bump

--- src/next.js/withFetch.ts
+++ src/next.js/withFetch.ts
@@ -2,15 +2,16 @@ import {ISource,}           from "@leight-core/api";
 import {getTokenUser}       from "@leight-core/server";
 import {GetServerSideProps} from "next";
 
-export const withFetch = <TKey extends string, TSource extends ISource<any, any, any, any>>(source: TSource, key: TKey, query: string): GetServerSideProps => async context => {
-	source.container.withUser(await getTokenUser(context));
+export const withFetch = <TKey extends string, TSource extends ISource<any, any, any, any>>(source: () => Promise<TSource>, key: TKey, query: string): GetServerSideProps => async context => {
+	const $source = await source();
+	$source.container.withUser(await getTokenUser(context));
 	if (!context.params?.[query]) {
 		return {
 			notFound: true,
 		};
 	}
 	try {
-		const item = await source.get(context.params[query] as string);
+		const item = await $source.get(context.params[query] as string);
 		if (!item) {
 			return {
 				notFound: true,
@@ -18,7 +19,7 @@ export const withFetch = <TKey extends string, TSource extends ISource<any, any,
 		}
 		return {
 			props: {
-				[key]: await source.mapper.toItem.map(item),
+				[key]: await $source.mapper.toItem.map(item),
 			}
 		};
 	} catch (e) {
