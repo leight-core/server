25c2efa9ded81f416d85d2870ff6b1d7c2ee1771
Marek Hanzal <marek.hanzal@x32.cz>
2022-11-07 13:40:09 +0100

    Common: Version bump

--- src/logger/utils.ts
+++ src/logger/utils.ts
@@ -1,6 +1,4 @@
-import winston         from "winston";
-import LokiTransport   from "winston-loki";
-import TransportStream from "winston-transport";
+import winston from "winston";
 
 const {transports} = winston;
 const {format}     = winston;
@@ -12,22 +10,6 @@ export type ILogLevel =
 	| "debug"
 	| "silly";
 
-interface LokiTransportOptions extends TransportStream.TransportStreamOptions {
-	host: string;
-	basicAuth?: string;
-	headers?: object;
-	interval?: number;
-	json?: boolean;
-	batching?: boolean;
-	labels?: object;
-	clearOnError?: boolean,
-	replaceTimestamp?: boolean,
-	gracefulShutdown?: boolean,
-	timeout?: number,
-
-	onConnectionError?(error: unknown): void
-}
-
 export const createConsole = () => new transports.Console({
 	format: format.combine(
 		format.colorize(),
@@ -36,38 +18,19 @@ export const createConsole = () => new transports.Console({
 	),
 });
 
-export const createLoki = (options?: Partial<LokiTransportOptions>) => new LokiTransport({
-	level: "debug",
-	host: (() => {
-		const url = process.env.LOKI_URL;
-		if (!url) {
-			throw new Error(`Missing Loki env variable "LOKI_URL", for example http://127.0.0.1:3100.`);
-		}
-		return url;
-	})(),
-	...options,
-});
-
 const createDefaultMeta = (version: string) => ({
 	labels: {
 		version,
 	},
 });
 
-const createDefaultLogger = (service: string, version: string, level: ILogLevel, withLoki: boolean) => ({
+const createDefaultLogger = (service: string, version: string, level: ILogLevel) => ({
 	level,
 	format:      winston.format.json(),
 	defaultMeta: createDefaultMeta(version),
 	transports:  [
-					 createConsole(),
-					 withLoki ? createLoki({
-						 labels: {
-							 version,
-							 service,
-						 },
-						 level,
-					 }) : null,
-				 ].filter(i => i) as TransportStream[],
+		createConsole(),
+	],
 });
 
 export interface IBootstrapLoggerRequest {
@@ -82,5 +45,4 @@ export const BootstrapLogger = (
 		loggers,
 		version = process.env.NEXT_PUBLIC_BUILD || "edge",
 		level = "info",
-		withLoki = true,
-	}: IBootstrapLoggerRequest) => loggers.map(name => winston.loggers.add(name, createDefaultLogger(name, version, level, withLoki)));
+	}: IBootstrapLoggerRequest) => loggers.map(name => winston.loggers.add(name, createDefaultLogger(name, version, level)));
